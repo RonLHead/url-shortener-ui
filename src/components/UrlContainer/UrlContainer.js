8869ab67b05e137c1224f5586b4d7ead92066e90
Ronald Head <ronaldlhead@gmail.com>
2022-04-26 10:34:44 -0600

    Refactor UrlContainer component

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './UrlContainer.css';
 
 const UrlContainer = props => {
-console.log(typeof props.urls)
-console.log(props)
   const urlEls = props.urls.map(url => {
 
     return (
